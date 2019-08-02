b411001c9a4168957314c481db25977c50f8505f
nguyenduysu <su.nguyenduy.nb@gmail.com>
2019-08-02 17:29:04 +0700

    update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,14 @@
 import {AppPage} from './app.po';
 import {browser, logging} from 'protractor';
-
+import { Employee } from '../../src/app/models/employee';
+export const mockEmployee =
+{
+    id: 2,
+    name: 'Nguyen Hien',
+    team: 'C#',
+    score: 600,
+    id_employee: 3339
+} as Employee;
 describe('workspace-project App', () => {
     let page: AppPage;
 
@@ -10,7 +18,7 @@ describe('workspace-project App', () => {
 
     it('should display welcome message', () => {
         page.navigateTo();
-        expect(page.getTitleText()).toEqual('Welcome to angular-demo!');
+        expect(page.getTitleText()).toEqual('Welcome to angular demo!');
     });
 
     afterEach(async () => {
@@ -19,5 +27,41 @@ describe('workspace-project App', () => {
         expect(logs).not.toContain(jasmine.objectContaining({
             level: logging.Level.SEVERE,
         } as logging.Entry));
+    });
+
+    describe('workspace-project employees page', () => {
+        it('should route to employees page', () => {
+            page.navigateTo();
+            page.getEmployeeButton().click();
+            expect(page.getTextTitleListEmployee()).toEqual('Danh sách nhân viên Fresher');
+        });
+
+        it('should display list of Employee Prehser', () => {
+            page.navigateToEmployee();
+            expect(page.getTextTitleListEmployee()).toEqual('Danh sách nhân viên Fresher');
+        });
+
+        it('should display button Thêm mới nhân viên', () => {
+            page.navigateTo();
+            expect(page.getButtonAddNewEmployee()).toBeTruthy();
+        });
+    });
+
+    describe('workspace-project employee detail page', () => {
+        it('should route to employee detail', () => {
+            page.navigateToEmployee();
+            page.getEmployeeById(11).click();
+            expect(page.getTextTitleEmployeeDetail()).toEqual('Thông tin nhân viên');
+        });
+
+        it('shuould display infor Emplyee', () => {
+            page.navigateToEmployeeDetail(11);
+            expect(page.getTextTitleEmployeeDetail()).toEqual('Thông tin nhân viên');
+        });
+
+        it('should display button go back', () => {
+            page.navigateToEmployeeDetail(mockEmployee.id);
+            expect(page.getButtonGoBack).toBeTruthy();
+        });
     });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,43 @@
-import {browser, by, element} from 'protractor';
-
+import {browser, by, element, ElementFinder} from 'protractor';
 export class AppPage {
+
     navigateTo() {
         return browser.get(browser.baseUrl) as Promise<any>;
     }
 
     getTitleText() {
         return element(by.css('app-root h1')).getText() as Promise<string>;
+    }
+
+    navigateToEmployee() {
+        return browser.get('/employees');
+    }
+
+    getTextTitleListEmployee() {
+        return element(by.css('app-employees h1')).getText() as Promise<string>;
+    }
+
+    getButtonAddNewEmployee() {
+        return element(by.css('#formInsert'));
+    }
+
+    getEmployeeButton() {
+        return element(by.css('[ng-reflect-router-link="employees"]'));
+    }
+
+    getEmployeeById(id: number): ElementFinder {
+        return element(by.css(`[ng-reflect-router-link="/employee,detail,${id}"]`));
+    }
+
+    getTextTitleEmployeeDetail() {
+        return element(by.css('app-employee-detail h1')).getText();
+    }
+
+    navigateToEmployeeDetail(id: number) {
+        return browser.get(`/employee/detail/${id}`);
+    }
+
+    getButtonGoBack() {
+        return element(by.css('.goBack'));
     }
 }
